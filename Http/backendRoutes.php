c3a44b837e2a208dacad07888a1c567745284b26
William <wavutes@gmail.com>
2018-12-08 22:49:16 -0400

    first commit

---
+++ Http/backendRoutes.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Routing\Router;
+/** @var Router $router */
+
+$router->group(['prefix' =>'/greentest'], function (Router $router) {
+// append
+});
---
+++ Providers/GreentestServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Modules\Greentest\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Modules\Core\Traits\CanPublishConfiguration;
+use Modules\Core\Events\BuildingSidebar;
+use Modules\Core\Events\LoadingBackendTranslations;
+use Modules\Greentest\Events\Handlers\RegisterGreentestSidebar;
+
+class GreentestServiceProvider extends ServiceProvider
+{
+    use CanPublishConfiguration;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerBindings();
+        $this->app['events']->listen(BuildingSidebar::class, RegisterGreentestSidebar::class);
+
+        $this->app['events']->listen(LoadingBackendTranslations::class, function (LoadingBackendTranslations $event) {
+            // append translations
+        });
+    }
+
+    public function boot()
+    {
+        $this->publishConfig('greentest', 'permissions');
+
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
+
+    private function registerBindings()
+    {
+// add bindings
+    }
+}
